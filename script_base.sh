ccb3320eec67a03fb8da78a64a3d93e603ff7c9f
Jean Lourenço <loginsocial@outlook.com>
2018-02-20 09:55:58 -0300

    Create script_base.sh

---
+++ script_base.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#script base para acesso ssh em massa, visando execução de comandos em qualquer tipo de equipamento.
+#É a base para qualquer automação utilizando SSH.
+#Fonte: https://youtu.be/6PFu746z2Dg
+#ips: deve ter um arquivo com esse nome contendo os ips a serem conectados.
+#comandos: deve ter um arquivo com esse nome contendo os comandos a serem executados.
+#O método abaixo utiliza chave publica/privada.
+#Para usar com parametro senha --> sshpass -p 'senha aqui' ssh -o StrictHostKeyChecking=no root@"$ip" "$(cat ./comandos)" 
+
+
+ips=$(cat ./ips)
+array_ips=($ips)
+
+for ip in ${array_ips[@]}
+do
+ssh-keygen -f "/home/$USER/.ssh/known_hosts" -R $ip
+ssh -t -o 'StrictHostKeyChecking no' -o ConnectTimeout=10 root@"$ip" "$(cat ./comandos)"
+done
